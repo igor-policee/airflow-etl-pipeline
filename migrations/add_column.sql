4adae1718e168705794ab5a6ad64bf9f43ca35dd
Igor Polishchuk <73423580+igor-policee@users.noreply.github.com>
2022-08-01 22:19:46 +0300

    edit comments

--- migrations/add_column.sql
+++ migrations/add_column.sql
@@ -1,10 +1,10 @@
--- Создание колонки "status" в таблице de.staging.user_order_log
+-- Creating column "status" in the table de.staging.user_order_log
 alter table
     staging.user_order_log
 add column if not exists
     "status" varchar(10);
 
--- Создание колонки "status" в таблице de.mart.f_sales
+-- Creating column "status" в таблице de.mart.f_sales
 alter table
     de.mart.f_sales
 add column if not exists
